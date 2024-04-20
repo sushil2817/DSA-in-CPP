97b06ef93e07cdffbd7cc18f4a49a6b05bdfe8c5
sushil2817 <tony281723@gmail.com>
2024-04-20 10:09:00 +0530

    solved find pivot in array with two methods 1) brute force method 2)Prefix method

--- BasicCPP/basicCPP.cpp
+++ BasicCPP/basicCPP.cpp
@@ -1,53 +1,118 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int countSetBit(int n){
+int countSetBit(int n)
+{
 
     int count = 0;
-    while(n>0){
-        int bit = n%2;
-         if(bit == 1){
-            count++;
-         }
-         n = n/2;
-    }
-    return count; 
-}
-// print all digit of an interger
-void printDigit(int num){
-    int digit ;
-    while(num>0){
-        digit = num%10;
-        num = num/10;
-        cout<<digit<<" ";
-    }
-}
-
-// with bitwise and 
-int setBits(int n){
-    int count = 0;
-    while(n>0){
-        int bit = (n&1);
-        if(bit==1){
+    while (n > 0)
+    {
+        int bit = n % 2;
+        if (bit == 1)
+        {
             count++;
         }
-        n = n>>1;
+        n = n / 2;
+    }
+    return count;
+}
+// print all digit of an interger
+void printDigit(int num)
+{
+    int digit;
+    while (num > 0)
+    {
+        digit = num % 10;
+        num = num / 10;
+        cout << digit << " ";
+    }
+}
+
+// with bitwise and
+int setBits(int n)
+{
+    int count = 0;
+    while (n > 0)
+    {
+        int bit = (n & 1);
+        if (bit == 1)
+        {
+            count++;
+        }
+        n = n >> 1;
     }
     return count;
 }
 
+int bruteForce(vector<int> &nums)
+{
+    for (int i = 0; i < nums.size(); i++)
+    {
+        int lsum = 0;
+        int rsum = 0;
 
-int main(){
-    
-       int n ;
-       cin>>n;
-       int setBitCount = countSetBit(n);
-       printDigit(n);
+        for (int j = 0; j < i; j++)
+        {
+            lsum += nums[j];
+        }
+        for (int j = i + 1; j < nums.size(); j++)
+        {
+            rsum += nums[j];
+        }
+        if (lsum == rsum)
+            return i;
+    }
+    return -1;
+}
+
+int bruteForceM2PrefixSum(vector<int>& nums){
+    vector<int>lsum(nums.size(),0);
+    vector<int>rsum(nums.size(),0);
+    // cal lsum array
+    for(int i=1;i<nums.size();i++){
+        lsum[i] = lsum[i-1]+nums[i-1];
+    }
+    // cal rsum array
+    for( int i = nums.size()-2;i>=0;i--){
+        rsum[i] = rsum[i+1]+nums[i+1]; 
+    }
+    // check krte hai
+
+    for(int i=0;i<nums.size();i++){
+        if(lsum[i]==rsum[i]) return i;
+    }
+     return -1; 
+}
+
+int main()
+{
+
+    // int arr[] = {1,7,3,6,5,6};
+    vector<int> arr;
+    arr.push_back(1);
+    arr.push_back(7);
+    arr.push_back(3);
+    arr.push_back(6);
+    arr.push_back(5);
+    arr.push_back(6);
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl
+         << "ans is : =  ";
+    cout << bruteForceM2PrefixSum(arr);
+    //    int n ;
+    //    cin>>n;
+    //    int setBitCount = countSetBit(n);
+    //    printDigit(n);
 
     //    cout<<setBitCount<<endl;
 
     // cout<<setBits(n);
-    
-    
+
     return 0;
 }
