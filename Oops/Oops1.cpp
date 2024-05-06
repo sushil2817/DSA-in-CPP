6967a1d00cc2bbb5c638c6cdb2ace3eb1788cf98
sushil2817 <tony281723@gmail.com>
2024-05-06 08:53:47 +0530

    learning Oops 'classes, Object, Access Moidifier, static and dyanamic memory allocation'

--- Oops/Oops1.cpp
+++ Oops/Oops1.cpp
@@ -5,34 +5,75 @@
 // class have always a size 1byte
 // ***********  smallest addresable memory in system is a    1Byte not 1 bit   ************
 
+//Paading -->> sabse bde data type ke nearest multiple  size ki padding add krte hai
+
+// Access modifier -> Public,Private,Protected
+// access modifier 
+
 #include<iostream>
 #include<string>
 #include<vector>
 using namespace std;
 
- class Student{
-    // int id;
-    // int age;
-    string name;
-    // int nos;
-    void study(){
-        cout<<"Studiying"<<endl;
-    }
-    void sleep(){
-        cout<<"sleeping"<<endl;
-    }
-    void bunk(){
-        cout<<"bunking"<<endl;
-    }
- };
+class Animal{
+ 
+ private:
+  int weight;
+
+  public:
+ // state or properties
+  int age;
+  string name;
+
+
+//   behaviour
+  void eat(){
+     cout<<"eating"<<endl;
+  }
+   void sleep(){
+      cout<<"sleeping"<<endl;
+   }
+
+   int getWeight(){
+       return weight;
+   }
+   int setWeight(int w){
+      weight = w;
+   }
+};
+
 
  int main(){
-   
-  //Paading -->> sabse bde data type ke nearest multiple  size ki padding add krte hai
 
-    cout<<sizeof(Student)<<endl;
-        // Student s1;
-        // Student s2;
+    // Object Creation
+
+    // Static
+    Animal ramesh;
+    ramesh.age = 12;
+    ramesh.name = "Ramesh";
+    cout<<"Age of ramesh is: "<<ramesh.age<<endl;
+    cout<<"Name of ramesh is: "<<ramesh.name<<endl;
      
+      ramesh.eat();
+      ramesh.eat();
+    
+     ramesh.setWeight(101);
+     cout<<ramesh.getWeight();
+
+      
+
+    // Dynamic memory
+       
+       Animal* suresh = new Animal;
+       (*suresh).age = 15;
+       (*suresh).name = "Suresh";
+   // alternate -> dynamic
+       suresh->age = 17;
+       suresh->name = "baby";
+       suresh->eat();
+       suresh->sleep();
+
+
+       
      return 0;
  }
