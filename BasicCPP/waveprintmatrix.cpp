eb8405f4f4f919306c6cef3a0b40387ed1dee1eb
sushil2817 <tony281723@gmail.com>
2024-05-06 23:36:07 +0530

    solving some questions

---
+++ BasicCPP/waveprintmatrix.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void wavePrintMatrix(vector<vector<int>>v){
+    int m = v.size();
+    cout<<m<<endl;
+    int n = v[0].size();
+    cout<<n<<endl;
+    for(int startCol = 0;startCol<n;startCol++){
+    //   even no of col -> Top to bottom
+         if((startCol & 1)==0){
+            for(int i=0;i<m;i++){
+                cout<<v[i][startCol]<<" ";
+            }
+         }else{
+            // odd no of col -> bottome to top
+            for( int i = m-1;i>=0;i--){
+                 cout<<v[i][startCol]<<" ";
+            }
+         }
+    }
+}
+
+int main(){
+
+      vector<vector<int>>v{
+        {1,2,3,4},{5,6,7,8},{9,10,11,12}
+      };
+
+      wavePrintMatrix(v);
+
+
+    return 0;
+}
