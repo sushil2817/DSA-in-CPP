7a488beee18b2b85305b73dfd2a9c8625c870d1c
sushil2817 <tony281723@gmail.com>
2024-05-07 08:56:10 +0530

    learning Oop

--- Oops/Oops1.cpp
+++ Oops/Oops1.cpp
@@ -23,7 +23,47 @@ class Animal{
   public:
  // state or properties
   int age;
-  string name;
+  string type;
+
+//default Constuctor
+  Animal(){
+   this->weight = 0;
+   this->age = 0;
+   this->type = "";
+   cout<<"constructor called"<<endl;
+  }
+
+
+//   Perameterised Constructor
+   
+    Animal(int age ){
+      this->age = age;
+      cout<<"Perametrised Constructor Called"<<endl;
+    }
+
+    Animal(int age, int weight){
+      this->age = age;
+      this->weight = weight;
+      cout<<"Peramertrised 2 Called"<<endl;
+    }
+
+    Animal(int age, int weight,string type){
+      this->age = age;
+      this->weight = weight;
+      this->type  = type;
+      cout<<"Peramertrised 3 Called"<<endl;
+    }
+
+
+
+    // copy Constructor
+    Animal(Animal &obj){
+      this->age = obj.age;
+      this->weight = obj.weight;
+      this->type = obj.type;
+      cout<<"I am inside copy constructor "<<endl;
+    }
+
 
 
 //   behaviour
@@ -37,8 +77,12 @@ class Animal{
    int getWeight(){
        return weight;
    }
-   int setWeight(int w){
-      weight = w;
+   int setWeight(int weight){
+      this->weight = weight;
+      // this points to current object
+   }
+   ~Animal(){
+      cout<<"I am inside destructor"<<endl;
    }
 };
 
@@ -48,30 +92,46 @@ class Animal{
     // Object Creation
 
     // Static
-    Animal ramesh;
-    ramesh.age = 12;
-    ramesh.name = "Ramesh";
-    cout<<"Age of ramesh is: "<<ramesh.age<<endl;
-    cout<<"Name of ramesh is: "<<ramesh.name<<endl;
+   //  Animal ramesh(10,82,"ramesh");
+
+   //  Animal a = ramesh;
+   //  ramesh.age = 12;
+   //  ramesh.name = "Ramesh";
+   //  cout<<"Age of ramesh is: "<<ramesh.age<<endl;
+   //  cout<<"Name of ramesh is: "<<ramesh.name<<endl;
      
-      ramesh.eat();
-      ramesh.eat();
+   //    ramesh.eat();
+   //    ramesh.eat();
     
-     ramesh.setWeight(101);
-     cout<<ramesh.getWeight();
+   //   ramesh.setWeight(101);
+   //   cout<<ramesh.getWeight();
+
+         // Destructor
+
+         Animal a;
+         a.age = 5;
+      
+      
 
       
 
     // Dynamic memory
        
-       Animal* suresh = new Animal;
-       (*suresh).age = 15;
-       (*suresh).name = "Suresh";
-   // alternate -> dynamic
-       suresh->age = 17;
-       suresh->name = "baby";
-       suresh->eat();
-       suresh->sleep();
+      //  Animal* suresh = new Animal(100);
+   //     (*suresh).age = 15;
+   //     (*suresh).name = "Suresh";
+   // // alternate -> dynamic
+   //     suresh->age = 17;
+   //     suresh->name = "baby";
+   //     suresh->eat();
+   //     suresh->sleep();
+
+   Animal*b = new Animal();
+   b->age = 12;
+
+   // manually des called
+
+   delete b;
 
 
        
