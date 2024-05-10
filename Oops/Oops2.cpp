8be5ecff161d22cbc7a9adf5dfe9c1d481d29afa
sushil2817 <tony281723@gmail.com>
2024-05-10 11:18:12 +0530

    understanding inheritance and it's 5 types

--- Oops/Oops2.cpp
+++ Oops/Oops2.cpp
@@ -4,43 +4,98 @@
 
 // 2) Inheritance ==> child classname : Access modifier parentlass Name {}
 
+/*   Base Class            Child CLass
+                        public     private    protected     
+       public           public       
+       private
+       protected
+*/
+
+/*      Type of Inheritance
+            1) Single
+            2) multi-level
+            3) multiple
+            4) hierachical
+            5) hybride
+*/
+
 
 #include <iostream>
 using namespace std;
 
-class Animal{
-//    private://(full encapsulation)
-  protected:
-   int age;
-   int weight;
-   void eat(){
-    cout<<"Eating"<<endl;
-   }
+  // single level---------------
+// class Animal{
+// //    private://(full encapsulation)
+//   public:
+//      int age;
 
-   int getAge(){
-    return this->age;
-   }
-   void setAge(int age){
-    this->age = age;
-   }
+// };
 
+
+// class Dog: public Animal{
+//          public:
+//          void print(){
+//           cout<<this->age;
+
+//          }
+// };
+
+
+// Multilevel-------------------
+
+// class Fruit{
+//   public:
+//   string name;
+// };
+
+// class Mango: public Fruit{
+//       public:
+//       int weight;
+// };
+
+// class Alphanso: public Mango{
+//        public:
+//        int sugarLevel;
+// };
+
+
+// multiple ------------------
+
+class A{
+  public:
+  int physic;
+};
+class B{
+   public:
+   int chemistry;
+};
+class C: public A, public B{
+    public:
+    int maths;
 };
 
-
-class Dog: private Animal{
-         public:
-         void print(){
-          cout<<this->age;
-
-         }
-};
 
 int main() {
 
-    Dog d1;
-    cout<<d1.age<<endl;
-    cout<<d1.weight<<endl;
-    d1.eat();
+//  multiple --------------
+  
+    C obj;
+    cout<<obj.chemistry<<" "<<obj.maths<<" "<<
+
+
+
+
+//  multilevel Inheritance-----------
+      // Alphanso a;
+
+      // cout<<a.name<<" "<<a.weight<<" "<< a.sugarLevel<<" "<<endl;
+
+// single------------
+    //  Dog d1;
+    //  cout<<d1.age;
+    //  d1.print();
+    // cout<<d1.age;
+    
     // d1.bark();
     // cout<<"Hello World!";
     return 0;
