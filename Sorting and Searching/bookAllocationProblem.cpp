aee4708e5153ba2415b602846af276966af5a8c4
sushil2817 <tony281723@gmail.com>
2024-05-17 00:12:05 +0530

    solving book allocation problem

--- Sorting and Searching/bookAllocationProblem.cpp
+++ Sorting and Searching/bookAllocationProblem.cpp
@@ -1,55 +1,63 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
- bool isPossibleSoln(int A[], int N, int M, int sol){
-        int pageSum = 0;
-        int c = 1;
-        for(int i=0;i<N;i++){
-            if(A[i]>sol){
+bool isPossibleSoln(int A[], int N, int M, int sol)
+{
+    int pageSum = 0;
+    int c = 1;
+    for (int i = 0; i < N; i++)
+    {
+        if (A[i] > sol)
+        {
+            return false;
+        }
+        if (pageSum + A[i] > sol)
+        {
+            c++;
+            //  pageSum = 0;
+            //  pageSum += A[i]
+            pageSum = A[i];
+            if (c > M)
+            {
                 return false;
             }
-            if(pageSum + A[i] > sol){
-                 c++;
-                //  pageSum = 0;
-                //  pageSum += A[i]
-                 pageSum = A[i];
-                 
-                 if(c > M){
-                     return false;
-                 }
-            }
-            else{
-                pageSum += A[i];
-            }
         }
-        return true;
+        else
+        {
+            pageSum += A[i];
+        }
     }
-    //Function to find minimum number of pages.
-    int findPages(int A[], int N, int M) 
+    return true;
+}
+// Function to find minimum number of pages.
+int findPages(int A[], int N, int M)
+{
+    // code here
+    if (M > N)
+        return -1;
+    int start = 0;
+    int end = accumulate(A, A + N, 0);
+    int ans = -1;
+
+    while (start <= end)
     {
-        //code here
-        if(M>N)return -1;
-        int start = 0;
-        int end = accumulate(A,A+N,0);
-        int ans = -1;
-        
-        while(start <= end){
-            int mid = (start+end)>>1;
-            if(isPossibleSoln(A,N,M,mid)){
-                ans = mid;
-                end= mid-1;
-            }else{
-                start = mid+1;
-            }
+        int mid = (start + end) >> 1;
+        if (isPossibleSoln(A, N, M, mid))
+        {
+            ans = mid;
+            end = mid - 1;
         }
-        return ans;
+        else
+        {
+            start = mid + 1;
+        }
     }
+    return ans;
+}
 
-int main(){
+int main()
+{
     int x = 124;
-
-
-
 
     return 0;
 }
