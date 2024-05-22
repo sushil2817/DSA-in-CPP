ad2ad7ea3556aa3bad9fea4c5a2fb209ccabe771
sushil2817 <tony281723@gmail.com>
2024-05-22 09:10:01 +0530

    solved Book allocation problem

--- Sorting and Searching/bookAllocationProblem.cpp
+++ Sorting and Searching/bookAllocationProblem.cpp
@@ -1,3 +1,67 @@
+// #include <iostream>
+// using namespace std;
+
+// bool isPossibleSoln(int A[], int N, int M, int sol)
+// {
+//     int pageSum = 0;
+//     int c = 1;
+//     for (int i = 0; i < N; i++)
+//     {
+//         if (A[i] > sol)
+//         {
+//             return false;
+//         }
+//         if (pageSum + A[i] > sol)
+//         {
+//             c++;
+//             //  pageSum = 0;
+//             //  pageSum += A[i]
+//             pageSum = A[i];
+//             if (c > M)
+//             {
+//                 return false;
+//             }
+//         }
+//         else
+//         {
+//             pageSum += A[i];
+//         }
+//     }
+//     return true;
+// }
+// // Function to find minimum number of pages.
+// int findPages(int A[], int N, int M)
+// {
+//     // code here
+//     if (M > N)
+//         return -1;
+//     int start = 0;
+//     int end = accumulate(A, A + N, 0);
+//     int ans = -1;
+
+//     while (start <= end)
+//     {
+//         int mid = (start + end) >> 1;
+//         if (isPossibleSoln(A, N, M, mid))
+//         {
+//             ans = mid;
+//             end = mid - 1;
+//         }
+//         else
+//         {
+//             start = mid + 1;
+//         }
+//     }
+//     return ans;
+// }
+
+// int main()
+// {
+//     int x = 124;
+
+//     return 0;
+// }
+
 #include <iostream>
 using namespace std;
 
@@ -11,11 +75,9 @@ bool isPossibleSoln(int A[], int N, int M, int sol)
         {
             return false;
         }
-        if (pageSum + A[i] > sol)
+        else if (pageSum + A[i] > sol)
         {
             c++;
-            //  pageSum = 0;
-            //  pageSum += A[i]
             pageSum = A[i];
             if (c > M)
             {
@@ -29,19 +91,23 @@ bool isPossibleSoln(int A[], int N, int M, int sol)
     }
     return true;
 }
-// Function to find minimum number of pages.
-int findPages(int A[], int N, int M)
+
+// A[] no. of pages
+// N no. of books
+// M no. of students
+int findPage(int A[], int N, int M)
 {
-    // code here
+
     if (M > N)
         return -1;
-    int start = 0;
-    int end = accumulate(A, A + N, 0);
-    int ans = -1;
 
+    int start = 0;
+    int end = accumulate(A, A - N, 0);
+
+    int ans = -1;
     while (start <= end)
     {
-        int mid = (start + end) >> 1;
+        int mid = (start + end) << 1;
         if (isPossibleSoln(A, N, M, mid))
         {
             ans = mid;
@@ -57,7 +123,6 @@ int findPages(int A[], int N, int M)
 
 int main()
 {
-    int x = 124;
 
     return 0;
 }
