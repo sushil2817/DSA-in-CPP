274add8e20b08ee551bdfc151938dc2eae529177
sushil2817 <tony281723@gmail.com>
2024-05-30 22:26:08 +0530

    learning doubly linked list

--- LinkedList/LinkedList2.cpp
+++ LinkedList/LinkedList2.cpp
@@ -27,14 +27,14 @@ void print(Node* head){
     }
 }
 
-void getLen(Node* head){
+int getLen(Node* head){
     Node* temp = head;
     int count = 0;
     while(temp!=NULL){
         count++;
         temp = temp->next;
     }
-    cout<<count;
+    return count;
 }
 
 void insertAtHead(Node* &head, Node* &tail, int data){
@@ -57,6 +57,60 @@ void insertAtHead(Node* &head, Node* &tail, int data){
     }
 }
 
+void insertAtTail(Node* &head,Node* &tail,int data){
+    // LL is empty
+    if(tail == NULL){
+        Node* newNode = new Node(data);
+        head = newNode;
+        tail = newNode;
+    }else{
+    // LL is not empty
+      Node* newNode = new Node(data);
+    //  tail ko new node se connect krna hai
+      tail->next = newNode;
+      newNode->prev  = tail;
+    //   tail ko update krna hai
+      tail = newNode;
+    }
+
+}
+
+void insertAtMiddle(Node* &head,Node* &tail, int data, int pos){
+    int len = getLen(head);
+    if(head == NULL){
+        // LL is empty
+        Node* newNode = new Node(data);
+        head = newNode;
+        tail = newNode;
+    }else{
+        // LL is not empty
+        if(pos == 1){
+            insertAtHead(head,tail,data);
+        }else if(pos == len+1){
+            insertAtTail(head,tail,data);
+        }else{
+        //step: 1 insert at middle
+        Node* newNode = new Node(data);
+        // step: 2 prev and curr pointer
+        Node* prevNode = NULL;
+        Node* currNode = head;
+        while(pos != 1){
+            pos--;
+            prevNode = currNode;
+            currNode = currNode->next;
+        }
+        // step: 3 update pointer
+           prevNode->next = newNode;
+           newNode->prev = prevNode;
+
+           newNode->next = currNode;
+           currNode->prev = newNode;
+        }
+    }
+}
+
+
+
 int main(){
   
 
@@ -68,8 +122,11 @@ int main(){
   insertAtHead(head,tail,30);
   insertAtHead(head,tail,20);
   insertAtHead(head,tail,10);
+  insertAtTail(head,tail,1000);
+  insertAtMiddle(head,tail,56,4);
 
   print(head);
+
 
 
 
