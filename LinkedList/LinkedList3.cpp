b5cb1aba1ee132e9c78d26020ab3b288308a6b9c
sushil2817 <tony281723@gmail.com>
2024-06-10 22:55:45 +0530

    solving some questions

--- LinkedList/LinkedList3.cpp
+++ LinkedList/LinkedList3.cpp
@@ -63,3 +63,24 @@
     }
 
 
+
+
+// Check cycle in a linked list
+// Leetcode 141
+
+ bool hasCycle(ListNode *head) {
+        map<ListNode*,bool>  table;
+        ListNode* temp = head;
+        while(temp!=NULL){
+            if(table[temp]==false){
+                table[temp]=true;
+            }else{
+                // cycle present 
+                return true;
+            }
+            temp = temp->next;
+
+        }
+        // loop not present;
+        return false;
+    }
