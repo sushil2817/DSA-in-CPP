bd4c12a2d16f7584aabbd2a3d0030dcdb85c9448
sushil2817 <tony281723@gmail.com>
2024-06-27 22:31:26 +0530

    solving some questions

--- LinkedList/LinkedList4.cpp
+++ LinkedList/LinkedList4.cpp
@@ -1,32 +1,48 @@
-#include<iostream>
-using namespace std; 
-// if some question solve with map trying to solve it with another method because interviewrs say try to do another method
-
+// check for loop 
+// if you solved some problem with MAP trying to solve that question with other method like fast and slow (toutais method)
 
 bool checkLoop(Node* &head){
     Node* slow = head;
     Node* fast = head;
 
     while(fast!=NULL){
-          fast = fast->next;
-          if(fast != NULL){
-            fast = fast>next
+        fast = fast->next;
+        if(fast!=NULL){
+            fast = fast->next;
             slow = slow->next;
-
-          }
-
-        //   check for loop
+        }
+        // check for loop
         if(fast == slow){
             return true;
         }
-
     }
     return false;
 }
-int main(){
 
+// Starting point off loop
 
+Node* getStartingPoint(Node* &head){
+   Node* slow = head;
+   Node* fast = head;
+   while(fast != NULL){
+         fast = fast->next;
+         if(fast!=NULL){
+            fast = fast->next;
+            slow = slow->next;
+         }
 
+         if(fast == slow){
+            break;
+         }
+   }
+    //  mtlb slow and fast meet kr chuke hai
+     slow = head;
 
-    return 0;
+    //  slow and fast -> 1 step
+    while(fast!=slow){
+        slow = slow->next;
+        fast = fast->next; 
+    }
+    // return starting point
+    return slow;
 }
