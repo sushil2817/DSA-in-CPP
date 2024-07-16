114bb8d80a2dfacdb9a1741b860ef5546af535b5
sushil2817 <tony281723@gmail.com>
2024-07-16 22:54:01 +0530

    solving some questions

---
+++ stack/insertAtBottom.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+void insertAtBottom(stack<int>&st,int &element){
+    // base case
+    if(st.empty()){
+        st.push(element);
+        return;
+    }
+    // ek case me solve krunga
+    int temp = st.top();
+    st.pop();
+
+    // baki recursion dekh lega
+    insertAtBottom(st,element);
+    // backtrack
+    st.push(temp);
+}
+
+int main()
+{
+    stack<int>st;
+
+    st.push(10);
+    st.push(20);
+    st.push(30);
+
+    int element = 400;
+
+    insertAtBottom(st,element);
+
+    while(!st.empty()){
+        cout<<st.top()<<" ";
+        st.pop();
+    }
+
+
+return 0;
+}
