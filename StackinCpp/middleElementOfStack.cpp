0598effaf9bbc83ea33fdc92cbce7447180d3375
sushil2817 <tony281723@gmail.com>
2024-07-16 22:22:43 +0530

    some questions of stack

---
+++ StackinCpp/middleElementOfStack.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+void solve(stack<int>&st, int& pos, int& ans){
+
+    if(pos == 1){
+        ans =  st.top();
+        return;
+    }
+
+    // 1 case hum solve krege
+    pos--;
+    int temp = st.top();
+    st.pop();
+
+    // resursion
+    solve(st,pos,ans);
+    // backtrack
+    st.push(temp);
+}
+
+int getMidddleElement(stack<int>& st){
+    int size = st.size();
+    if(st.empty()){
+        return -1;
+    }else{
+        // stack is not empty
+        // odd
+        int pos = 0;
+          if(size & 1){
+            // odd
+            pos = size/2+1;
+        }else{
+            // even
+            pos = size/2;
+        }
+
+        int ans = -1;
+        solve(st,pos,ans);
+        return ans;
+    }
+
+}
+
+int main()
+{
+ stack<int>st;
+//  st.push(10);
+//  st.push(20);
+//  st.push(30);
+//  st.push(40);
+//  st.push(50);
+//  st.push(60);
+
+ int mid = getMidddleElement(st);
+ cout<<"middle "<<mid;
+
+
+
+return 0;
+}
