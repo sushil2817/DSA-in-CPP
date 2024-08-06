9934d1bf90eb9f71301feb00910652ddc8ef48de
Sushil Kumawat <tony281723@gmail.com>
2024-08-06 23:14:05 +0530

    stack commit

--- stack/insertInSortedStack.cpp
+++ stack/insertInSortedStack.cpp
@@ -2,30 +2,32 @@
 #include<stack>
 using namespace std;
 
-void insertSorted(stack<int>& st, int element){
-// base case
-// jab bhi top access kro tb el ghnti bjni chahiye ki stack empty to hi hai
+void insertSorted(stack<int> &st, int element)
+{
+    // base case
+    // jab bhi top access kro tb el ghnti bjni chahiye ki stack empty to hi hai
 
-if(st.empty() || element > st.top()){
-    st.push(element);
-    return;
-}
+    if (st.empty() || element > st.top())
+    {
+        st.push(element);
+        return;
+    }
 
-// 1 case solve
-int temp = st.top();
-st.pop();
+    // 1 case solve
+    int temp = st.top();
+    st.pop();
 
-// recusion
+    // recusion
 
-insertSorted(st,element);
+    insertSorted(st, element);
 
-// backtracking
-st.push(temp);
+    // backtracking
+    st.push(temp);
 }
 
 int main()
 {
-    stack<int>st;
+    stack<int> st;
 
     st.push(10);
     st.push(20);
@@ -33,18 +35,14 @@ int main()
     st.push(40);
 
     int element = 35;
-    
-    insertSorted(st,element);
-    
+
+    insertSorted(st, element);
 
     while (!st.empty())
     {
-        cout<<"element: "<<st.top()<<endl;
+        cout << "element: " << st.top() << endl;
         st.pop();
     }
-    
-    
-    
-    
-return 0;
+
+    return 0;
 }
