caabf4c44faaa5485459db49cf351fe8ec83048e
Sushil Kumawat <tony281723@gmail.com>
2024-08-08 22:19:09 +0530

    Solving some questions

---
+++ stack/nextSmallerElement.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+
+vector<int> nextSmallerElement(int* arr, int size, vector<int> &ans){
+    stack<int>st;
+    st.push(-1);
+
+    for(int i=size-1;i>=0;i--){
+        int curr = arr[i];
+        // and find kro current element ke liye
+        while(st.top() >= curr){
+            st.pop();
+        }
+        ans[i] = st.top();
+        st.push(curr);
+    }
+
+    return ans;
+}
+
+int main(){
+
+int arr[5] = {8,4,6,2,3};
+int size = 5;
+
+vector<int>ans(size);
+
+ans = nextSmallerElement(arr,size,ans);
+for(auto i:ans){
+    cout<<i<<" ";
+}
+
+return 0;
+}
