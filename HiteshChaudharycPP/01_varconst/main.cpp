915b703473173827720dd771e8f506ef3a36eb3e
Sushil Kumawat <tony281723@gmail.com>
2024-09-09 08:49:04 +0530

    basic of cpp

---
+++ HiteshChaudharycPP/01_varconst/main.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int score; //declare
+    score = 110; // assignment 
+
+    const int uid = 232323;
+
+    int balance = 10000;
+    balance = 20000;
+
+    // uid = 1212;
+
+    // cout<<"Welcome to chai with cpp1"<<endl;
+    
+    // cout<<"Welcome to chai with cpp2"<<endl;
+    // cout<<"Welcome to chai with cpp3"<<endl;
+
+    return 0;
+}
