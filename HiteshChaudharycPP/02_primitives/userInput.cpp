d9f7285c2ed98f52baca374037cb02eef5994425
Sushil Kumawat <tony281723@gmail.com>
2024-09-19 22:39:30 +0530

    some basics of cpp

--- HiteshChaudharycPP/02_primitives/userInput.cpp
+++ HiteshChaudharycPP/02_primitives/userInput.cpp
@@ -6,12 +6,10 @@ int main(){
     string userTea;
     int teaQuantity;
     cout<<"What would like to order in tea? \n";
-
     getline(cin, userTea);
     // ask for quantity
     cout<<"how many cups of "<<userTea<<" would you like to have? ";
     cin>>teaQuantity;
-
     cout<<teaQuantity;
     cout<<" "<<userTea;
 
