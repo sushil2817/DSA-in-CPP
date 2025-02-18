8e565c67d883374903b8b9718359f73215d48cc1
sushil2817 <tony281723@gmail.com>
2025-02-18 08:28:00 +0530

    creationa array

--- DSA2025/Array/array.cpp
+++ DSA2025/Array/array.cpp
@@ -2,6 +2,25 @@
 using namespace std;
 int main(){
 
+    // array creation
+    // int arr[101];
+    // char ch[20];
+    // bool flags[10];
+    // long num[900];
+    // short s[1000];
+
+    // cout<<"array created successfully"<<endl;
+
+
+    int a = 5;
+    cout<<"Address of a : "<<&a <<endl;
+    cout<<"Size of a "<<sizeof(a)<<endl;
+
+    int arr[100];
+    cout<<"Base addres of arr is : "<<&arr<<endl;
+    cout<<"Base address of Arr is : "<<arr<<endl;
+    cout<<"Size of arr : "<<sizeof(arr)<<endl;
+
 
 
 return 0;
