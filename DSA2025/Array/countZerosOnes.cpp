600671151339f18fee2ac05e6d5281d303941543
sushil2817 <tony281723@gmail.com>
2025-03-12 08:54:36 +0530

    solving some array questions

---
+++ DSA2025/Array/countZerosOnes.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+void countZeroOne(int arr[], int size){
+    int zeroCount = 0;
+    int oneCount = 0;
+
+    for(int i=0;i<size;i++){
+        if(arr[i] == 0){
+            zeroCount++;
+        }else{
+            oneCount++;
+        }
+    }
+    cout<<"Zero Count: "<<zeroCount<<endl;
+    cout<<"One Count: "<<oneCount<<endl;
+}
+
+
+int main(){
+
+    int arr[] = {0,0,0,1,1,0,1,0,1,0,1,0,1,1,1,1,0};
+
+    int size = 17;
+
+    countZeroOne(arr,size);
+
+return 0;
+}
