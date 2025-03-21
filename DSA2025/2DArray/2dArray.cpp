de3f158ae3dc03defe335520a5d352474df4d137
sushil2817 <tony281723@gmail.com>
2025-03-21 08:27:08 +0530

    learning about 2D Array

---
+++ DSA2025/2DArray/2dArray.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+void print2dArray(int arr[][4], int row, int col){
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+int main(){
+
+    // create a 2D array
+    // int arr[3][3];
+
+    int arr[3][4] = { {1,2,3,4},
+                    {5,6,7,8},
+                    {9,10,11,12}
+                };
+
+    int crr[][4] = { {1,2,3,4},
+                    {5,6,7,8},
+                    {9,10,11,12}
+                };
+    // Number of column dena bhut jrurt hai
+
+    // print-2dArr
+    int row = 3;
+    int col = 4;
+    print2dArray(arr,row,col);
+
+return 0;
+}  
