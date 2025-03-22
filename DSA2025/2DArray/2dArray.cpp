63bcd79ef30ea6e898c96e8d3aee14f3fcc0d02f
sushil2817 <tony281723@gmail.com>
2025-03-22 22:06:28 +0530

    learning about 2D Array

--- DSA2025/2DArray/2dArray.cpp
+++ DSA2025/2DArray/2dArray.cpp
@@ -10,25 +10,20 @@ void print2dArray(int arr[][4], int row, int col){
     }
 }
 int main(){
-
     // create a 2D array
     // int arr[3][3];
-
     int arr[3][4] = { {1,2,3,4},
                     {5,6,7,8},
                     {9,10,11,12}
                 };
-
     int crr[][4] = { {1,2,3,4},
                     {5,6,7,8},
                     {9,10,11,12}
                 };
     // Number of column dena bhut jrurt hai
-
     // print-2dArr
     int row = 3;
     int col = 4;
     print2dArray(arr,row,col);
-
 return 0;
 }  
