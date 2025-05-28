0862cf69f93b2cd5f3d4929ba1f48b4bea5947a4
sushil2817 <tony281723@gmail.com>
2025-05-28 08:37:48 +0530

    Solverd a sorting question called find missing element using B.S.

---
+++ DSA2025/Searching $ Sorting/findMissingElement.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int findMissingElement(int arr[], int size){
+    int s = 0;
+    int e = size-1;
+    int mid = s+(e-s)/2;
+    int ans = -1;
+
+    while(s <= e){
+        int diff = arr[mid]-mid;
+
+        if(diff == 1){
+            // right me jao
+            s = mid+1;
+        }else {
+            // ans store
+            ans = mid;
+            e = mid-1;
+        }
+        mid = s+(e-s)/2;
+    }
+    return ans+1;
+}
+
+int main(){
+
+    int arr[] = {1,3,4,5,6,7,8};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    cout<<size<<endl;
+    
+    cout<<"Missing Element is  "<<findMissingElement(arr,size);
+
+return 0;
+}
