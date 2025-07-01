d177deedb9976c611516dc9cf2d6ba59e7983d2c
sushil2817 <tony281723@gmail.com>
2025-07-01 08:33:40 +0530

    bubble sorting

---
+++ DSA2025/Searching & Sorting/sorting/bubbleSort.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void bubbleSort(vector<int> arr, int n){
+
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){ // < for decresing order
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+
+    
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
+int main(){
+
+    vector<int> arr = {5,4,3,2,1};
+    int n = arr.size();
+
+    cout<<n<<endl;
+    bubbleSort(arr, n);
+
+
+
+return 0;
+}
