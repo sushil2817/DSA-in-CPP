af8449c3c94a056457f59d9fc8592d9334aa9cc1
sushil2817 <tony281723@gmail.com>
2025-07-14 08:37:33 +0530

    learn about char array

--- DSA2025/Character/characterArrayString.cpp
+++ DSA2025/Character/characterArrayString.cpp
@@ -1,6 +1,44 @@
 #include<iostream>
+#include<string.h>
 using namespace std;
-int main(){
+int findLenth(char ch[], int size){
+    int length = 0;
+    int index = 0;
+
+    while(ch[index] != '\0'){
+        index++;
+    }
     
+    for(int i=0;i<size;i++){ 
+        if(ch[i] == '\0'){
+            // ruk jao
+            break;
+        }else{
+            length++;
+        }
+    }
+    return length;
+}
+int main(){
+
+    char ch[100];
+    cin>>ch;
+    int len = findLenth(ch,100);
+    cout<<"Lenght of string is: "<<len<<endl;
+    cout<<"Lenght of string is: "<<strlen(ch)<<endl;
+    // char ch[100];
+    
+    // // input
+    // cin.getline(ch,100);
+
+    // // print
+    // cout<<"Printing the char array "<<ch;
+    // for(int i=0;i<10;i++){
+    //     cout<<"At Index: "<<i<<" "<<ch[i]<<endl;
+    // }
+    // char temp = ch[6];
+    // int val = (int)temp;
+    // cout<<"Printitng int value: "<<val<<endl;
+
 return 0;
 }
