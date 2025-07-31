09c60fdd11ee9880f799751d169e90badcafe8ac
sushil2817 <tony281723@gmail.com>
2025-07-31 08:58:47 +0530

    learn about variables and data types

--- DSA-Youtube/first.cpp
+++ DSA-Youtube/first.cpp
@@ -2,4 +2,38 @@
 using namespace std;
 int main(){
     cout<<"Hello World:)"<<endl;
+
+    // Data Type and Varibables
+
+    // int a = 123;
+    // cout<<a<<endl;
+    // char b = 'V';
+    // bool bl = true;
+    // cout<<bl<<endl;
+    // float f = 1.23;
+    // cout<<f<<endl;
+    // double d = 12.34;
+    // cout<<d<<endl;
+
+    // int size = sizeof(bl);
+    // int size2 = sizeof(b);
+
+    // int ans = 0;
+    // if(sizeof(bl) == sizeof(b)){
+    //     cout<<"Ans shi h"<<endl;
+    // }else{
+    //     cout<<"Ans glt hai"<<endl;
+    // }
+    // cout<<"Size of bl is : "<<size<<endl;
+    // cout<<"Size of c is : "<<size2<<endl;
+
+
+    int x = 'a';
+    cout<<x<<endl;
+    char ch = 98;
+
+    cout<<ch<<endl;
+
+    char bh = 123456;
+    cout<<bh<<endl;
 }
