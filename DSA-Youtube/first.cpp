03c31eca3dfaa73dfaf07cdd726fdff6f71a4292
sushil2817 <tony281723@gmail.com>
2025-08-01 21:59:24 +0530

    learn about operators

--- DSA-Youtube/first.cpp
+++ DSA-Youtube/first.cpp
@@ -4,7 +4,6 @@ int main(){
     cout<<"Hello World:)"<<endl;
 
     // Data Type and Varibables
-
     // int a = 123;
     // cout<<a<<endl;
     // char b = 'V';
@@ -14,10 +13,8 @@ int main(){
     // cout<<f<<endl;
     // double d = 12.34;
     // cout<<d<<endl;
-
     // int size = sizeof(bl);
     // int size2 = sizeof(b);
-
     // int ans = 0;
     // if(sizeof(bl) == sizeof(b)){
     //     cout<<"Ans shi h"<<endl;
@@ -27,13 +24,41 @@ int main(){
     // cout<<"Size of bl is : "<<size<<endl;
     // cout<<"Size of c is : "<<size2<<endl;
 
+    // int x = 'a';
+    // cout<<x<<endl;
+    // char ch = 98;
+    // cout<<ch<<endl;
+    // char bh = 123456;
+    // cout<<bh<<endl;
 
-    int x = 'a';
-    cout<<x<<endl;
-    char ch = 98;
+    // unsigned int a = -123;
+    // // cout<<a<<endl;
 
-    cout<<ch<<endl;
+    // double a = 2.0/5;
+    // cout<<a<<endl;
 
-    char bh = 123456;
-    cout<<bh<<endl;
+    // int a = 2;
+    // int b = 3;
+    // bool first = (a==b);
+    // cout<<first<<endl;
+
+    // bool second = (a>b);
+    // cout<<second<<endl;
+    
+    // bool third = (a<b);
+    // cout<<third<<endl;
+    
+    // bool fourth = (a>=b);
+    // cout<<first<<endl;
+    
+    // bool fifht = (a<=b);
+    // cout<<fifht<<endl;
+
+    // bool sixth = (a!=b);
+    // cout<<sixth<<endl;
+
+    int a = 0;
+    cout<<!a<<endl;
+
+
 }
