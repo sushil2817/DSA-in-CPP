86aeed3d159a36a8098d1cc11830dca2d0a37a17
sushil2817 <tony281723@gmail.com>
2025-08-06 23:22:27 +0530

    binary to decimal system

---
+++ DSA-Youtube/binaryDecimalNoSystem.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int ans = 0;
+    int multiplier = 1;
+
+    while (n != 0) {
+        int bit = n & 1;
+        ans += bit * multiplier;
+        multiplier *= 10;
+        n = n >> 1;
+    }
+
+    cout << "Answer is: " << ans << endl;
+    return 0;
+}
--- DSA-Youtube/operators/first.cpp
+++ DSA-Youtube/operators/first.cpp
@@ -10,6 +10,17 @@ int main(){
     cout<<"~b "<<(~b)<<endl;
     cout<<"a^b "<<(a^b)<<endl;
 
+    cout<<(17>>1)<<endl;
+    cout<<(17>>2)<<endl;
+    cout<<(19<<1)<<endl;
+    cout<<(19<<2)<<endl;
+
+    int i = 7;
+
+    cout<<(++i)<<endl; //8
+    cout<<(i++)<<endl; //8
+    cout<<(i--)<<endl; //9
+    cout<<(--i)<<endl; //7
 
 return 0;
-}
+} 
