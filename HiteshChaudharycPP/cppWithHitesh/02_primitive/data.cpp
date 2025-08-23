f833eabb3f7074bc9a7f595510f6116ef1e52eca
sushil2817 <tony281723@gmail.com>
2025-08-23 23:05:05 +0530

    learn about variables

---
+++ HiteshChaudharycPP/cppWithHitesh/02_primitive/data.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+        int teaLeaves = 50;
+        float waterTemperature = 85.5;
+        double priceOfTea = 299.9999;
+        char teaGreade = 'A';
+        bool isTeaReady = false;
+
+        cout<<priceOfTea<<endl;
+
+
+
+return 0;
+}
