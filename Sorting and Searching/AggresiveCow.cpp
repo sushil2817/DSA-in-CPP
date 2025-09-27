54470bfeaf3ec91affd8bae8819f7b785a267687
sushil2817 <tony281723@gmail.com>
2025-09-27 19:55:05 +0530

    refactor AggresiveCow.cpp for improved code clarity and organization

--- Sorting and Searching/AggresiveCow.cpp
+++ Sorting and Searching/AggresiveCow.cpp
@@ -1,45 +1,45 @@
 #include<iostream>
 using namespace std;
 
+int c = 1;
+int pos = stals[0];
 
- int c = 1;
-      int pos = stalls[0];
-      
-      for(int i=1;i<stalls.size();i++){
-          if(stalls[i] - pos>=mid ){
-              c++;
-              pos = stalls[i] ;// one more cow has been placed'
-              
-          }
-          if(c==k) return true;
-      }
-      return false;
-  }
+for(int i = 1;i < stalls.size();i++) {
+    if (stalls[i] - pos >= mid) {
+        c++;
+        pos = stalls[i];// one more cow has been placed'
 
-    int solve(int n, int k, vector<int> &stalls) {
-    
-        // Write your code here
-        sort(stalls.begin(),stalls.end());
-        int start = 0;
-        int end = stalls[stalls.size()-1] - stalls[0];
-        
-        int ans = -1;
-        
-        while(start<=end){
-            int mid  = start+(end-start)/2;
-            if(isPossible(stalls,k,mid)){
-                ans = mid;
-                start = mid+ 1;
-            }else{
-                end = mid-1;
-            }
-            
-        }
-        return ans;
     }
-int main(){
-
-
-
-    return 0;
+    if (c == k) return true;
+    return false;
 }
+
+
+int solve(int n, int k, vector<int>& stalls) {
+
+      // Write your code here
+      sort(stalls.begin(), stalls.end());
+      int start = 0;
+      int end = stalls[stalls.size() - 1] - stalls[0];
+
+      int ans = -1;
+
+      while (start <= end) {
+          int mid = start + (end - start) / 2;
+          if (isPossible(stalls, k, mid)) {
+              ans = mid;
+              start = mid + 1;
+          }
+          else {
+              end = mid - 1;
+          }
+
+      }
+      return ans;
+  }
+  int main() {
+
+
+
+      return 0;
+  }
