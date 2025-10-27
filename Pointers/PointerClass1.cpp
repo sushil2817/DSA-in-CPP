a308929617a07877b0db62434e60aa64738fb078
sushil2817 <tony281723@gmail.com>
2025-10-27 21:50:54 +0530

    refactor PointerClass1.cpp for improved code formatting and consistency

--- Pointers/PointerClass1.cpp
+++ Pointers/PointerClass1.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 using namespace std;
-int main(){
-
+int main() {
     // int a = 5;
     // cout<<a<<endl;
     // cout<<"address of a : "<<&a<<endl;
@@ -11,25 +10,16 @@ int main(){
     // cout<<"Accessing: "<<*ptr<<endl;
     // cout<<"ptr Accessing: "<<ptr<<endl;
     // cout<<"ptr address: "<<&ptr<<endl;
-
-
     // int* ptr = nullptr;
     // cout<<*ptr<<endl;
-
     int a = 5;
     int* p = &a;
-    cout<<&a<<endl;
-    cout<<p<<endl;
-    p=p+1;
-
-    cout<<p<<endl;
-    
-    cout<<*p<<endl;
-
-    int arr[5] = {10,20,30,40,50};
-    arr = arr+1; // why
-
-
-
-return 0;
+    cout << &a << endl;
+    cout << p << endl;
+    p = p + 1;
+    cout << p << endl;
+    cout << *p << endl;
+    int arr[5] = { 10,20,30,40,50 };
+    arr = arr + 1; // why
+    return 0;
 }
