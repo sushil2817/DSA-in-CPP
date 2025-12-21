1d89087e280d2e1ea03d561975db07d3bfa12e50
sushil2817 <tony281723@gmail.com>
2025-12-21 12:03:48 +0530

    changes

--- 00DSA/Array/findDuplicate.cpp
+++ 00DSA/Array/findDuplicate.cpp
@@ -1,24 +1,15 @@
 #include<iostream>
 using namespace std;
 int main(){
-
     int arr[5] = {3,1,0,0,2};
-
     int size = sizeof(arr)/sizeof(arr[0]);
-    
     int ans = 0;
-
     for(int i=0;i<size;i++){
-
         ans = ans^arr[i];
-    
     }
     for(int i=1;i<size;i++){
-    
         ans = ans^i;
-    
     }
-    
     cout<<"Duplicate element is : "<<ans<<endl;
 
 return 0;
