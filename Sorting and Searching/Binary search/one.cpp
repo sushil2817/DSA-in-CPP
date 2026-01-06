77fa9dde852c77d2139cfe4594e3b2a81ef4ed99
sushil2817 <tony281723@gmail.com>
2026-01-06 22:54:53 +0530

    refactor one.cpp to improve code formatting by removing unnecessary blank lines

--- Sorting and Searching/Binary search/one.cpp
+++ Sorting and Searching/Binary search/one.cpp
@@ -2,11 +2,10 @@
 using namespace std;
 
 int binarySearch(int arr[], int size, int key){
+
     int start = 0;
     int end = size-1;
-
     int mid = (start+end)/2;
-
     while(start<=end){
         if(arr[mid] == key){
             return mid;
@@ -18,17 +17,15 @@ int binarySearch(int arr[], int size, int key){
         mid = (start+end)/2;
     }
     return -1;
+    
 }
-
 
 int main(){
 
     int even[6] = {2,4,6,8,12,18};
     int odd[6] = {3,8,11,14,16};
-
     int ans1 = binarySearch(even,6,1);
     int ans2= binarySearch(odd,5,20);
-
     cout<<ans1<<endl;
     cout<<ans2<<endl;
 return 0;
