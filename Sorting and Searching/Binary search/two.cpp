ef264acc1317f2f4b35da887c0902ee2147fd565
sushil2817 <tony281723@gmail.com>
2026-01-07 21:06:14 +0530

    changes

---
+++ Sorting and Searching/Binary search/two.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+int firstOcc(int arr[], int size, int key){
+    int s = 0;
+    int e = size-1;
+    int mid = s+(e-s)/2;
+    int ans  = -1;
+    while(s<=e){
+        if(arr[mid] == key){
+            ans = mid;
+            e = mid-1;
+        }else if(arr[mid] < key){
+            // right
+            s = mid+1;
+        }else{
+            e = mid-1;
+        }
+
+        mid = s+(e-s)/2;
+    }
+}
+int LastOcc(int arr[], int size, int key){
+    int s = 0;
+    int e = size-1;
+    int mid = s+(e-s)/2;
+    int ans  = -1;
+    while(s<=e){
+        if(arr[mid] == key){
+            ans = mid;
+            s = mid+1;
+        }else if(arr[mid] < key){
+            // right
+            s = mid+1;
+        }else{
+            e = mid-1;
+        }
+
+        mid = s+(e-s)/2;
+    }
+}
+int main(){
+
+    int arr[] = {1,2,3,3,3,3,3,3,3,3,3,3,3,3,5};
+    int size =  sizeof(arr)/sizeof(arr[0]);
+    cout<<size<<endl;
+    int ans = firstOcc(arr,size,3);
+    int ans1 = LastOcc(arr,size,3);
+    cout<<"first Occ of 3 is "<<ans<<endl;
+    cout<<"last Occ of 3 is "<<ans1<<endl;
+return 0;
+}
