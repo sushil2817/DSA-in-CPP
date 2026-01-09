c5ba4676f667b3b8988bc93739811eae44cb3878
sushil2817 <tony281723@gmail.com>
2026-01-09 22:34:07 +0530

    changes

---
+++ Sorting and Searching/Binary search/three.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int foundPeakElement(int arr[], int size){
+    int s = 0, e = size-1;
+    int mid = s+(e-s)/2;
+    while(s<=e){
+        
+    }
+}
+int main(){
+    int arr[] = {1,3,4,5,1};
+return 0;
+}
