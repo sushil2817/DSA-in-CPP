b66d676881e159c3569550798296cad528cb0427
sushil2817 <tony281723@gmail.com>
2026-01-16 00:28:14 +0530

    peak mountain array

--- Sorting and Searching/Binary search/three.cpp
+++ Sorting and Searching/Binary search/three.cpp
@@ -4,11 +4,21 @@ using namespace std;
 int foundPeakElement(int arr[], int size){
     int s = 0, e = size-1;
     int mid = s+(e-s)/2;
-    while(s<=e){
-        
+    while(s<e){
+        if(arr[mid]<arr[mid+1]){
+            s = mid+1;
+        }else{
+            e = mid;
+        }
+        mid = s+(e-s)/2;
     }
+    return arr[s];
 }
 int main(){
-    int arr[] = {1,3,4,5,1};
-return 0;
+    int arr[] = {3,4,5,1};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    cout<<"lenght of array "<<size<<endl;
+    int ans = foundPeakElement(arr,size);
+    cout<<"Peak Element is "<<ans<<endl;
+    return 0;
 }
