e8ad11a4c0493364fba99d13c6aabb68e04ec51f
sushil2817 <tony281723@gmail.com>
2026-03-23 23:17:16 +0530

    starting again c++

---
+++ CPPNew/main.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // variable define
+    // int age = 25;
+    // cout<<"My age is "<<age<<endl;
+
+    // decalaration
+    int age;
+    // cout<<age;
+    age=23;
+    // mainuplation/ updation
+
+    age = 25;
+
+    int count = 5;
+    
+    float share = 3.15;
+
+    char alphabet = 'z';
+
+    double weight = 55.69887;
+
+    bool isMale = true;
+    bool isChild = 1;
+
+    
+    
+    return 0;
+}
