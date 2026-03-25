5ff385e2074414d3295b654cf09ec7362a75820c
sushil2817 <tony281723@gmail.com>
2026-03-25 22:47:50 +0530

    add main.cpp for budget evaluation program

---
+++ CPPNew/control flow/main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+
+    int budget;
+    cout<<"Enter Your budget: "<<endl;
+
+    cin>>budget;
+
+    if(budget > 2000000){
+        cout<<"You can buy Scorpio"<<endl;
+    }else{
+        cout<<"Gareeb Aadmi 😂";
+    }
+
+return 0;
+}
