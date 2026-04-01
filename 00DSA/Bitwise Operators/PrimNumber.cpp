6dee49cdbfba48f49a7969d10c5bdf893bdea049
sushil2817 <tony281723@gmail.com>
2026-04-02 00:13:59 +0530

    Solve prime number and some questions

--- 00DSA/Bitwise Operators/PrimNumber.cpp
+++ 00DSA/Bitwise Operators/PrimNumber.cpp
@@ -2,19 +2,35 @@
 using namespace std;
 int main(){
 
-    int a = 7;
-    bool isPrime = 1;
-    for(int i=2;i<a;i++){
-        if((a%i) == 0){
-            cout<<"not a prime number";
-            isPrime = 0;
-            break;
+    // int a = 7;
+    // bool isPrime = 1;
+    // for(int i=2;i<a;i++){
+    //     if((a%i) != 0){
+    //         cout<<"a prime number";
+    //         isPrime = 0;
+    //         break;
+    //     }
+    // }
+    // if(isPrime == 1){
+    //     cout<<a<<" is a prime number";
+    // }
+
+    // for(int i=0;i<5;i++){
+    //     cout<<"Hi "<<endl;
+    //     cout<<"Hey "<<endl;
+    //     continue;
+    //     cout<<"Reply toh karde "<<endl;
+    // }
+
+    for(int i=0;i<5;i++){
+        for(int j=i;j<=5;j++){
+            if(i+j == 10){
+                cout<<"Breal";
+                break;
+            }
+            cout<<i<<" "<<j<<endl;
         }
     }
-    if(isPrime == 1){
-        cout<<a<<" is a prime number";
-    }
-
 
 return 0;
 }
