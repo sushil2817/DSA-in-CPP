23805ac21a87b693f7b5da9f11e2c2ae4985ccf5
sushil2817 <tony281723@gmail.com>
2026-04-22 09:17:03 +0530

    format code for better readability in findDuplicate.cpp

--- 00DSA/Array/findDuplicate.cpp
+++ 00DSA/Array/findDuplicate.cpp
@@ -1,12 +1,17 @@
 #include<iostream>
 using namespace std;
 int main(){
+    
     int arr[5] = {3,1,0,0,2};
+    
     int size = sizeof(arr)/sizeof(arr[0]);
+    
     int ans = 0;
+    
     for(int i=0;i<size;i++){
         ans = ans^arr[i];
     }
+
     for(int i=1;i<size;i++){
         ans = ans^i;
     }
