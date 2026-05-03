a1f95b3fba50e0c6edfc8c4fcc6f65f2527d3c17
sushil2817 <tony281723@gmail.com>
2026-05-03 21:34:16 +0530

    refactor: improve code formatting for better readability in multiple files

--- 00DSA/Array/pairSum.cpp
+++ 00DSA/Array/pairSum.cpp
@@ -9,28 +9,19 @@ int main(){
     vector<vector<int>>ans;
     int n = sizeof(arr)/sizeof(arr[0]);
     cout<<"this is n: "<<n<<endl;
-
     cout<<"this is size: "<<sizeof(arr)<<endl;
-    
     for(int i=0;i<n;i++){
-
         for(int j=i+1;j<n;j++){
-
             if(arr[i]+arr[j] == sum){
-
                 // vector<int>temp;
                 // temp.push_back(min(arr[i],arr[j]));
                 // temp.push_back(max(arr[i],arr[j]));'
-
                 ans.push_back({min(arr[i],arr[j]),max(arr[i],arr[j])});
-
             }
         }
     }
     sort(ans.begin(),ans.end());
-
     for(int i=0;i<ans.size();i++){
-
         cout<<ans[i][0]<<" "<<ans[i][1]<<" ";
         
     }
