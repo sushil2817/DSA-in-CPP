f1081b91cc43adc29f4093b7f0a595f1172f2a4d
sushil2817 <tony281723@gmail.com>
2026-05-25 20:33:07 +0530

    changes

--- 00DSA/Decimal Number System/first.cpp
+++ 00DSA/Decimal Number System/first.cpp
@@ -8,12 +8,19 @@ int main(){
     int ans = 0;
     int i = 0;
     while(n != 0){
+
         int bit = n & 1;
+        
         cout<<"Bit "<<bit<<endl;
+        
         ans = (bit * pow(10,i) ) + ans;
+        
         cout<<"Ans "<<ans<<endl; 
+        
         n = n >> 1;
+        
         cout<<"N "<<n<<endl; 
+        
         i++;
     }
 
