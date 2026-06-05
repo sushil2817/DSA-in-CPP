443687615fb008ee638c946f3c9f57ce5802781d
sushil2817 <tony281723@gmail.com>
2026-06-05 06:00:16 +0530

    format code by adding whitespace for better readability

--- 00DSA/Bitwise Operators/1281Leetcode.cpp
+++ 00DSA/Bitwise Operators/1281Leetcode.cpp
@@ -6,9 +6,13 @@ int main(){
     int product = 1;
     int sum = 0;
     while (n != 0){
+
             int rem = n%10;
+
             product = product*rem;
+
             sum =  sum + rem;
+            
             n=n/10;
         }
         cout<<product<<endl;
--- 00DSA/Decimal Number System/first.cpp
+++ 00DSA/Decimal Number System/first.cpp
@@ -10,12 +10,15 @@ int main(){
     while(n != 0){
 
         int bit = n & 1;
+
         cout<<"Bit "<<bit<<endl;
+        
         ans = (bit * pow(10,i) ) + ans;
         cout<<"Ans "<<ans<<endl; 
         n = n >> 1;
         cout<<"N "<<n<<endl; 
         i++;
+
     }
 
     cout<<"Answer is "<<ans<<endl;
