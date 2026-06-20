ec26d0825e9ad016543ce8869adfbd854ce34f86
sushil2817 <tony281723@gmail.com>
2026-06-20 19:28:12 +0530

    solved some questions

--- 00DSA/Array/arrAndFunction.cpp
+++ 00DSA/Array/arrAndFunction.cpp
@@ -9,20 +9,15 @@ void print(int arr[], int size){
 }
 
 int inc(int crr[]){
-
     crr[0] = crr[0]+10;
     cout<<"outside main"<<endl;
     print(crr,2);
-
 }
 
 int main(){
-
     int arr[] = {5,6};
     int size = 2;
-    
     inc(arr);
-    
     cout<<"Inside main"<<endl;
     print(arr,size);
 
