d3151047be4141e568d2066dffebd4f295e8e06e
sushil2817 <tony281723@gmail.com>
2026-06-22 23:29:47 +0530

    changes

--- 00DSA/Array/pairSum.cpp
+++ 00DSA/Array/pairSum.cpp
@@ -14,7 +14,9 @@ int main(){
         for(int j=i+1;j<n;j++){
             if(arr[i]+arr[j] == sum){
                 // vector<int>temp;
+                
                 // temp.push_back(min(arr[i],arr[j]));
+                
                 // temp.push_back(max(arr[i],arr[j]));'
                 ans.push_back({min(arr[i],arr[j]),max(arr[i],arr[j])});
             }
