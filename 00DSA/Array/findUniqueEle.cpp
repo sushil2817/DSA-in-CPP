a287374c0523902942647af52e8c975b072c849d
sushil2817 <tony281723@gmail.com>
2026-06-24 20:36:17 +0530

    find unique

--- 00DSA/Array/findUniqueEle.cpp
+++ 00DSA/Array/findUniqueEle.cpp
@@ -12,22 +12,15 @@ int findUnique(vector<int>arr){
 }
 
 int main(){
-
-
     int n;
     cout<<"Enter the size of array "<<endl;
     cin>>n;
-
     vector<int>arr(n);
     cout<<"Enter the elements "<<endl;
     for(int i=0;i<arr.size();i++){
         cin >> arr[i];
     }
-
     int uniqueElement = findUnique(arr);
-
     cout<<"Unique Element is "<<uniqueElement<<endl;
-
-
 return 0;
 }
