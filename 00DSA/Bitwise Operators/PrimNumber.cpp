28e716c328779a9796dd6284dbdf81f46afd807f
sushil2817 <tony281723@gmail.com>
2026-06-24 18:43:10 +0530

    prime number

--- 00DSA/Bitwise Operators/PrimNumber.cpp
+++ 00DSA/Bitwise Operators/PrimNumber.cpp
@@ -14,14 +14,12 @@ int main(){
     // if(isPrime == 1){
     //     cout<<a<<" is a prime number";
     // }
-
     // for(int i=0;i<5;i++){
     //     cout<<"Hi "<<endl;
     //     cout<<"Hey "<<endl;
     //     continue;
     //     cout<<"Reply toh karde "<<endl;
     // }
-
     for(int i=0;i<5;i++){
         for(int j=i;j<=5;j++){
             if(i+j == 10){
