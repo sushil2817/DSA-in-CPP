1bb8ddb14d9ec2b121afcca498abe18924015a1b
sushil2817 <tony281723@gmail.com>
2026-07-02 02:21:37 +0530

    code changes

--- 00DSA/Bitwise Operators/first.cpp
+++ 00DSA/Bitwise Operators/first.cpp
@@ -15,24 +15,19 @@ int main(){
     cout<<(17>>2)<<endl; //4
     cout<<(19<<1)<<endl; //38
     cout<<(19<<2)<<endl; //76
-
     int a,b = 1;
     a = 10;
-
     if(++a)
         cout<<b; // a = 11, b = 1
     else
         cout<< ++b;
-
     int a = 1,b = 2;
-
     if(a-- > 0 || ++b >2 ){ // a = 0, b=2
         cout<<"Stage 1 - Inside IF"<<endl; // print
     }else{
         cout<<"Stage 2 - Inside else";
     }
     cout<< a <<" "<< b <<endl; 
-
     int number = 3;
     cout<<25*(++number)<<endl; //100
 
