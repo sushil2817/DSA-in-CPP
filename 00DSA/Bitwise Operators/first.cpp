740446ca42c28b67182e8bb45d3120c813e5db19
sushil2817 <tony281723@gmail.com>
2026-07-03 19:22:43 +0530

    changes in first.cpp

--- 00DSA/Bitwise Operators/first.cpp
+++ 00DSA/Bitwise Operators/first.cpp
@@ -4,13 +4,11 @@ using namespace std;
 int main(){
     int a = 4;
     int b = 6;
-
     cout<<"a&b : "<< (a&b) <<endl;
     cout<<"a|b : "<< (a|b) <<endl;
     cout<<"~a : "<< ~a <<endl;
     cout<<"~b : "<< ~b <<endl;
     cout<<"a^b : "<< (a^b) <<endl;
-
     cout<<(17>>1)<<endl; //8
     cout<<(17>>2)<<endl; //4
     cout<<(19<<1)<<endl; //38
@@ -30,11 +28,8 @@ int main(){
     cout<< a <<" "<< b <<endl; 
     int number = 3;
     cout<<25*(++number)<<endl; //100
-
     int a = 1, b = a++, c = ++a;
-
     cout<<b<<endl; // 1
     cout<<c<<endl; // 3
-
     return 0;
 }
