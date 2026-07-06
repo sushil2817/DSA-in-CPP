aec1a4b3946261e73f3ab16982ccc26a6bedd2be
sushil2817 <tony281723@gmail.com>
2026-07-06 17:28:56 +0530

    revers array solved

--- 00DSA/Array/reverseArr.cpp
+++ 00DSA/Array/reverseArr.cpp
@@ -6,13 +6,11 @@ void printArr(int arr[], int size){
         cout<<arr[i]<<" ";
     }
 }
-
 void reverseArr(int arr[],int size){
     int start = 0;
     int end = size-1;
     while (start < end)
     {
-
         swap(arr[start],arr[end]);
         start++;
         end--;
@@ -25,7 +23,6 @@ int main(){
 
     int arr[] = {0,1,2,3,4,5,6,7,8,9,10};
     int size = sizeof(arr)/sizeof(arr[0]);
-
     reverseArr(arr,size);
 
 return 0;
