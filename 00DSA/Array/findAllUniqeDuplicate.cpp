52c40c507a2147096a9ce7d7707fbffd4870b258
sushil2817 <tony281723@gmail.com>
2026-07-13 22:44:04 +0530

    find all unique

--- 00DSA/Array/findAllUniqeDuplicate.cpp
+++ 00DSA/Array/findAllUniqeDuplicate.cpp
@@ -4,13 +4,16 @@ using namespace std;
 int main(){
 
     int arr[6] = {1,2,2,2,3,4};
+    
     int arr1[4] = {2,2,3,3};
 
     int n = sizeof(arr)/sizeof(arr[0]);
+    
     int m = sizeof(arr1)/sizeof(arr1[0]);
 
     int i=0,j=0;
     vector<int>ans;
+    
     while(i<n && j<m){
         if(arr[i]<arr1[j]){
             i++;
