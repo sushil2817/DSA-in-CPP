1ea98389ff628844c210a98024906d231c331b3a
sushil2817 <tony281723@gmail.com>
2026-07-19 23:46:44 +0530

    first file

--- 00DSA/Array/one.cpp
+++ 00DSA/Array/one.cpp
@@ -3,10 +3,15 @@ using namespace std;
 
 int main(){
     int arr[] = {1,2,3,4,5,6,7,8,9,10};
+    
     // &arr[0] = 104;
+    
     int size = sizeof(arr)/sizeof(arr[0]);
+    
     cout<<size<<endl;
+
     // cout<<&arr[0]<<endl<<&arr<<endl<<arr<<endl;
+    
     for(int i=0;i<size;i++){
         cout<<endl;
         cout<<"Arrary "<<i<<" Element Address "<<&arr[i]<<endl;
