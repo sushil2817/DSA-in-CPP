99adb8bd1b196b33f7613145f721d66c6b0e2e37
sushil2817 <tony281723@gmail.com>
2026-07-22 17:53:41 +0530

    changes

--- 00DSA/Array/findAllUniqeDuplicate.cpp
+++ 00DSA/Array/findAllUniqeDuplicate.cpp
@@ -1,34 +1,35 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-int main(){
+int main() {
 
-    int arr[6] = {1,2,2,2,3,4};
-    
-    int arr1[4] = {2,2,3,3};
+    int arr[6] = { 1,2,2,2,3,4 };
 
-    int n = sizeof(arr)/sizeof(arr[0]);
-    
-    int m = sizeof(arr1)/sizeof(arr1[0]);
+    int arr1[4] = { 2,2,3,3 };
 
-    int i=0,j=0;
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int m = sizeof(arr1) / sizeof(arr1[0]);
+
+    int i = 0, j = 0;
     vector<int>ans;
-    
-    while(i<n && j<m){
-        if(arr[i]<arr1[j]){
+
+    while (i < n && j < m) {
+        if (arr[i] < arr1[j]) {
             i++;
         }
-        else if(arr[i]==arr1[j]){
+        else if (arr[i] == arr1[j]) {
             ans.push_back(arr[i]);
-            i++,j++;
-        }else if(arr[i]>arr1[j]){
+            i++, j++;
+        }
+        else if (arr[i] > arr1[j]) {
             j++;
         }
     }
 
-    for(int i=0;i<ans.size();i++){
-        cout<<ans[i]<<" ";
+    for (int i = 0;i < ans.size();i++) {
+        cout << ans[i] << " ";
     }
 
-return 0;
+    return 0;
 }
