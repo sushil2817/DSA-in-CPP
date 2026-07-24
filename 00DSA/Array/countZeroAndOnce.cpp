27f4d8a9571c140ff4bbf3d52365bf170cb0d508
sushil2817 <tony281723@gmail.com>
2026-07-24 18:54:33 +0530

    change

--- 00DSA/Array/countZeroAndOnce.cpp
+++ 00DSA/Array/countZeroAndOnce.cpp
@@ -2,10 +2,8 @@
 using namespace std;
 
 int countZeroAndOnce(int arr[], int size){
-
     int Zero = 0;
     int Once = 0;
-
     for(int i=0;i<size;i++){
         if(arr[i] == 0){
             Zero++;
@@ -14,23 +12,14 @@ int countZeroAndOnce(int arr[], int size){
             Once++;
         }
     }
-
     cout<<"Zero "<<Zero<<endl;
     cout<<"Once "<<Once<<endl;
 
 }
 
-
 int main(){
-
-
     int arr[] = {0,1,1,1,1,1,0,0,0,0,0,0,1,1,1,3,4,5,2,5,0,11,1,0};
     int size = sizeof(arr)/sizeof(arr[0]);
-
-
     countZeroAndOnce(arr,size);
-
-
-
 return 0;
 }
