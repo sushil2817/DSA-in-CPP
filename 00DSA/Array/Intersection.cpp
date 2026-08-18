e1e2c39147c976104434e8bbe484dce5303a622a
sushil2817 <tony281723@gmail.com>
2026-08-18 23:46:07 +0530

    intersection

--- 00DSA/Array/Intersection.cpp
+++ 00DSA/Array/Intersection.cpp
@@ -5,10 +5,15 @@ using namespace std;
 int main(){
 
     vector<int> ans;
+    
     int arr1[4] = {1,2,3,4};
+
     int arr2[2] = {3,4};
+
     int n = sizeof(arr1)/sizeof(arr1[0]);
+
     int m = sizeof(arr2)/sizeof(arr2[0]);
+
     for(int i=0;i<n;i++){
             int element = arr1[i];
             for(int j=0;j<m;j++){
@@ -19,6 +24,7 @@ int main(){
                 }
             }
     }
+
     for(int i=0;i<ans.size();i++){
         cout<<ans[i]<<" ";
     }
