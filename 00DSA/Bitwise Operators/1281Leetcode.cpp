5a390aaa99ddd14b35740eb1da74d3748b9f14a4
sushil2817 <tony281723@gmail.com>
2026-08-20 05:38:25 +0530

    leetcode questions

--- 00DSA/Bitwise Operators/1281Leetcode.cpp
+++ 00DSA/Bitwise Operators/1281Leetcode.cpp
@@ -1,12 +1,10 @@
 #include<iostream>
 using namespace std;
 int main(){
-
     int n = 234;
     int product = 1;
     int sum = 0;
     while (n != 0){
-
             int rem = n%10;
             product = product*rem;
             sum =  sum + rem;
