bdb50e619ba5f7738d856a0991d99853edcaef9f
sushil2817 <tony281723@gmail.com>
2026-09-01 23:19:55 +0530

    learn about 2d matrex

--- 00DSA/2DArray/2d.cpp
+++ 00DSA/2DArray/2d.cpp
@@ -4,6 +4,19 @@
 using namespace std;
 
 int main() {
-    cout<<"code run for 2d matrix";
+    // decalre 2d array
+    int arr[3][3];
+
+    int brr[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+
+    // cout<<brr[2][2]<<endl;
+
+    for(int i=0;i<3;i++){
+        for (int j=0;j<3;j++){
+        cout<<brr[j][i]<<" ";
+        }
+        cout<<endl;
+    }
+
     return 0;
 }
