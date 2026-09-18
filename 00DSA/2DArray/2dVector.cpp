6ff85c994f065120374b1d97d7676705aa596ab5
sushil2817 <tony281723@gmail.com>
2026-09-18 23:32:25 +0530

    changes

--- 00DSA/2DArray/2dVector.cpp
+++ 00DSA/2DArray/2dVector.cpp
@@ -4,23 +4,6 @@
 using namespace std;
 
 int main() {
-    // vector < vector<int> > v;
-
-    // vector<int> a{1,2,3};
-    // vector<int> b{4,5,6};
-    // vector<int> c{7,8,9};
-
-    // v.push_back(a);
-    // v.push_back(b);
-    // v.push_back(c);
-
-    // for(int i=0;i<v.size();i++){
-    //     for(int j=0;j<v[0].size();j++){
-    //         cout<<v[i][j]<<" ";
-    //     }
-    //     cout<<endl;
-    // }
-    
     int row = 3;
     int col = 5;
     vector<vector<int>>arr(row,vector<int>(col,108));
@@ -30,7 +13,5 @@ int main() {
             }
             cout<<endl;
     }
-
-
     return 0;
 }
